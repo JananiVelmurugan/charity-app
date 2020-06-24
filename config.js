fc8dc4418b2512aa91a49defeaf013563223da13
Janani <jananise@gmail.com>
2020-06-24 12:55:58 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+let config1 = {
+  host: "localhost",
+  user: "root",
+  password: "surya123",
+  database: "crowdfund_db"
+};
+
+// let config = {
+//   host: "13.235.178.171",
+//   user: "surya",
+//   password: "Surya@123",
+//   database: "charityapp_db"
+// };
+let config = {
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "charityapp_db",
+}
+module.exports = config;
+
+
